b01ed08a7b18139fedb0eb027466e6df9edabbcf
Zander Sacay <zandersacay@gmail.com>
2023-07-18 17:35:47 +0800

    Added click event listener to pixels to change their color on click

--- js/script.js
+++ js/script.js
@@ -16,8 +16,20 @@ function createGrid(numOnSide) {
         pixel.className = "pixel";
         pixel.style.width = `${dimension}px`;
         pixel.style.height = `${dimension}px`;
+        pixel.addEventListener("click", () => {
+            if (!pixel.style.backgroundColor) {
+                pixel.style.backgroundColor = "#b4d7ff";
+            } else {
+                pixel.style.backgroundColor = "";
+            }
+
+        })
         grid.appendChild(pixel);
     }
+}
+
+function colorPixel(color) {
+
 }
 
 gridSizeInput.addEventListener("blur", () => {
